07605b305d3f9ec8edff630fb7501e45ee8eb12e
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2023-02-27 11:28:12 +0100

    Create ToDoList.sol

---
+++ Solidity/ToDoList.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract TodoList {
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    event TaskCreated(uint id, string content, bool completed);
+    event TaskCompleted(uint id, bool completed);
+    event TaskDeleted(uint id);
+
+    mapping(uint => Task) public tasks;
+    uint public taskCount;
+
+    constructor() {
+        taskCount = 0;
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
+    }
+
+    function deleteTask(uint _id) public {
+        require(_id <= taskCount && _id > 0, "Invalid task ID");
+        delete tasks[_id];
+        emit TaskDeleted(_id);
+    }
+}
